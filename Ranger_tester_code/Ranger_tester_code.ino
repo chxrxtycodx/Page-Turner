43a184f7bddbf68126d927d342e126875fd9042a
chritycodx <62725473+chxrxtycodx@users.noreply.github.com>
2025-04-10 23:45:25 -0400

    Added Ranger implementation

---
+++ Ranger_tester_code/Ranger_tester_code.ino
@@ -0,0 +1,34 @@
+/*
+ * HC-SR04 example sketch
+ *
+ * https://create.arduino.cc/projecthub/Isaac100/getting-started-with-the-hc-sr04-ultrasonic-sensor-036380
+ *
+ * by Isaac100
+ */
+
+const int trigPin = 14;
+const int echoPin = 15;
+
+float duration, distance;
+
+void setup() {
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration*.0343)/2;
+  if (distance <= 30){
+    Serial.print("Triggered, Distance: ");
+    Serial.println(distance);
+    delay(100);
+  }
+}
